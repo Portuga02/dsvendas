1dcd0d58b6f673a6197b08b8e31548a9fbf75fea
=Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2021-05-05 16:59:50 -0300

    configurando templates

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer () {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/savio-gomes-da-silva-4b2307141/" target="_blank" rel="noreferrer">Sávio Gomes da Silva</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">saviogomesdasilva@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
