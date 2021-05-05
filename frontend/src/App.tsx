583a6c9389dff96ef8177285dfbfd88ac496ddaf
=Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2021-05-05 17:30:25 -0300

    Statics charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,34 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/Navbar";
 
 function App() {
   return (
     <>
+
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Olá mundo</h1>
+        <h1 className="text-primary">Dashboard - Vendass</h1>
+
+        <div className="row py-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Porcentagem de Sucesso (%%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Porcentagem de Sucesso (%%)</h5>       <DonutChart />
+          </div>
+        </div>
+
+        <div className=" row py-3">
+          <h2 className="text-primary">Todas às vendas</h2>
+        </div>
+
         <DataTable />
+
+
       </div>
 
       <Footer />
